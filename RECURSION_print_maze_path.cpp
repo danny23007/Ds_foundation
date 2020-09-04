4ed810d8fe10c169e27d52ba20d70b0d77c17b04
danny23007 <57324330+danny23007@users.noreply.github.com>
2020-09-04 10:02:54 +0530

    Create RECURSION_print_maze_path.cpp
    https://www.pepcoding.com/resources/online-java-foundation/recursion-on-the-way-up/print-maze-paths-official/ojquestion

---
+++ RECURSION_print_maze_path.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+	void print_maze(int sr,int sc,int dr,int dc,string output){
+	if(sr>dr || sc>dc){
+		return ;
+	}
+	if(sr==dr &&sc==dr){
+		cout<<output<<endl;
+		return;
+	}
+	
+		print_maze(sr,sc+1,dr,dc,output+"h");
+	
+	
+		print_maze(sr+1,sc,dr,dc,output+"v");
+	
+}
+int main(){
+	int dr,dc;
+	
+	cin>>dr>>dc;
+	string output=" ";
+	print_maze(1,1,dr,dc,output);
+	return 0;
+}
